6baae9923b240b83a63a32972b51c4bdd3270bfb
Annie <amwarren33@gmail.com>
2024-07-01 17:00:57 -0500

    adding GPU capability

--- pde_cont_2d_hex/pde_cont_2d_hex.m
+++ pde_cont_2d_hex/pde_cont_2d_hex.m
@@ -1,9 +1,13 @@
 
+verbose = true;
+plotting = true;
+gpuON = false;
+
 % set up parameters
-n=128;% discretization size
+n=256;% discretization size
 eps=0.01; % regularizing viscosity- smaller eps requires larger n ~ 1/sqrt(eps)
-threshold = 0.001;
-ds=1e-1; % secant step size
+threshold = 0.01;
+ds=1e-3; % secant step size
 M =2; % size of domain in units of 2pi
 L = M*pi; 
 y1 = linspace(-L, L, n+1)'; y1=y1(1:end-1); %n points total, end is periodic point
@@ -47,20 +51,45 @@ laplace = (4/3)*(-k.^2 - k.*l - l.^2);
 V = cos(Y1)+cos(Y2)+cos(Y1-Y2);
 Vhat = fft2(V).*((4*pi*pi)/(n*n));
 
-% preconditioner for gmres
-%Dlin = spdiags(reshape((laplace-1),n*n,1),0,n*n,n*n); % linearization of potential differential operator (-1 prevents div by 0) 
-pc= @(duvec) [reshape(ifft2(fft2(reshape(duvec(1:end-4),n,n))./(laplace-1),'symmetric'),n*n,1);duvec(end-3:end)]; % preconditioner
-
 %initial conditions
 u0=1; %initial concentration
 uinit = u0*ones(n,n);%+0.9*(cos(Y1)+cos(Y2)+cos(Y1-Y2));
 uinit=uinit.*((n*n)/sum(sum(uinit))); %preserving average
 
-figure(11)
-surf(X1,X2,uinit,EdgeColor="none")
-    view(2)
-    axis([upperLeftbound(1) lowerRightbound(1) ylowlim yuplim threshold 5])
-    daspect([1 1 1])
+%%% GPU data transfers %%%
+if gpuON
+    n = gpuArray(n);
+    eps = gpuArray(eps);
+    threshold = gpuArray(threshold);
+    D1x1 = gpuArray(D1x1);
+    D1x2 = gpuArray(D1x2);
+    laplace = gpuArray(laplace);
+    Vhat = gpuArray(Vhat);
+    uvec = gpuArray(uvec);
+    tangentvec = gpuArray(tangentvec);
+    ntol = gpuArray(ntol);
+    ngmrestol = gpuArray(ngmrestol);
+    nnitermax = gpuArray(nnitermax);
+    nminstep = gpuArray(nminstep);
+    cutoff = gpuArray(cutoff);
+    contsteps = gpuArray(contsteps);
+    errorsinarow = zeros(1,1,"gpuArray");
+    mu_list = zeros(1,contsteps,"gpuArray");
+    vac_list = zeros(1,contsteps,"gpuArray");
+end
+
+% preconditioner for gmres
+%Dlin = spdiags(reshape((laplace-1),n*n,1),0,n*n,n*n); % linearization of potential differential operator (-1 prevents div by 0) 
+pc= @(duvec) [reshape(ifft2(fft2(reshape(duvec(1:end-4),n,n))./(laplace-1),'symmetric'),n*n,1);duvec(end-3:end)]; % preconditioner
+
+
+if plotting = true
+    figure(11)
+    surf(X1,X2,uinit,EdgeColor="none")
+        view(2)
+        axis([upperLeftbound(1) lowerRightbound(1) ylowlim yuplim threshold 5])
+        daspect([1 1 1])
+end
 
 mu=1/(sqrt(3)*pi*pi)-0.0075; % mu bifurcation parameter
 sx=0; % s for drift (compensating for extra phase condition), in x and y
@@ -78,6 +107,8 @@ ntol=1e-4;
 ngmrestol=1e-6;
 nnitermax= 35;
 nminstep=-1e-8;
+
+%%%% GPU commands %%%%
 
 % do first newton
 duvec=uvec;
@@ -98,7 +129,7 @@ inf_list=[];
 vac_list=[];
 l_list=[];
 h=figure(1);
-w=waitbar(0,'forming vacuum...');
+%w=waitbar(0,'forming vacuum...');
 
 vac_pts = 0;
 vac_pts_max = 12;
@@ -118,12 +149,14 @@ for contcount=1:contsteps
         ds=ds/2;
         display(['newton did not converge, reducing stepsize to ' num2str(ds)])
     else
-        if true
+        if verbose
             disp([num2str(newtonflag.iter) ' newton iterations'])
             disp([num2str(newtonflag.nresidual) ' residual after newton'])
         end
         tangentvec=uvec-uoldvec;tangentvec=tangentvec/norm(tangentvec);
-        set(0,'CurrentFigure',h)
+
+        if plotting = true;
+            set(0,'CurrentFigure',h)
             u = reshape(uvec(1:end-4),n,n);
             subplot(2,1,1)
             surf(X1,X2,u,EdgeColor="none")
@@ -131,26 +164,27 @@ for contcount=1:contsteps
             axis([upperLeftbound(1) lowerRightbound(1) ylowlim yuplim threshold 5])
             daspect([1 1 1])
             title(['$\mu=$' num2str(uvec(end)) ', $|u|_\infty$=' num2str(norm(uvec(1:end-4),'inf')) ' $ds=$' num2str(ds)],'interpreter','latex')
-            drawnow
             subplot(2,1,2)
             surf(X1,X2,u,EdgeColor="none")
             axis([upperLeftbound(1) lowerRightbound(1) ylowlim yuplim threshold 5])
             daspect([1 1 1])
+            drawnow
+        end    
+
         mu_list=[mu_list;uvec(end)];
         inf_list=[inf_list;norm(uvec(1:end-4),'inf')];
 
         % calculate size of vacuum
-        cutoff = 0.001;
-        vacuum = uvec(1:end-4) < cutoff;
+        vacuum = uvec(1:end-4) < threshold;
         vacuum_size = sum(vacuum)*4*pi*pi/(n*n);
         if vacuum_size > 0
             vac_pts = vac_pts+1;
-            waitbar(vac_pts/vac_pts_max,w,'taking vacuum data...')
+            %waitbar(vac_pts/vac_pts_max,w,'taking vacuum data...')
         end
-        vac_radius = sqrt(vacuum_size/(2*pi));
+        %vac_radius = sqrt(vacuum_size/(2*pi));
         disp(['vacuum size...' num2str(vacuum_size)])
         vac_list = [vac_list;vacuum_size];
-        l_list = [l_list;vac_radius];
+        %l_list = [l_list;vac_radius];
 
         if newtonflag.iter >5
             ds=ds/2; % decrease step size if newton not converging
@@ -159,27 +193,18 @@ for contcount=1:contsteps
             ds=min(1,ds*1.2); % increase stepsize if newton performing well
         end
     end
-    %if max(u(1:end-3))>2.3
-        %display("reached breakpoint")
-        %break
-    %end
-    if vac_pts == vac_pts_max
+    if max(u(1:end-3))>6
+        display("reached breakpoint")
         break
     end
 end
 
 figure(33)
+subplot(2,1,1)
 plot(mu_list,inf_list,'.-')
 xlabel(['$\mu$'],'Interpreter','latex')
 ylabel(['$|u|_\infty$'],'Interpreter','latex')
-
-
-figure(34)
-plot(mu_list,l_list,'.-')
-xlabel(['$\mu$'],'Interpreter','latex')
-ylabel('l')
-hold on
-mu_list_pred = linspace(1/(pi*pi),max(mu_list),512);
-prediction = nthroot(2*pi*pi*pi*(mu_list_pred-1/(pi*pi)),4);
-plot(mu_list_pred,prediction,'.-')
-axis([0.1013 max(mu_list)+0.0001 0 max([max(l_list) max(prediction)+0.05])])
+subplot(2,1,2)
+plot(vac_list.^2,mu_list,'.-')
+xlabel('$A^2$','Interpreter','latex')
+ylabel('$\mu$','Interpreter','latex')
